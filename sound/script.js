1007db5fd1dbe3206f0f843614ae126861246205
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-03-17 23:41:17 -0400

    added library

--- sound/script.js
+++ sound/script.js
@@ -1,18 +1,24 @@
-let mySound;
-function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('sounds/lane8.mp3');
-}
+
+let wave;
 
 function setup() {
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(canvasPressed);
-  background(220);
-  text('tap here to play', 10, 20);
+  createCanvas(1000, 300);
+  wave = new p5.Oscillator();
+  wave.setType('sine');
+  wave.amp(1);
+  wave.freq(300);
+  wave.start();
 }
 
-function canvasPressed() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
-  mySound.play();
+
+function draw() {
+  background(0);
+  let freq = map(mouseX, 0, width, 40, 880);
+  wave.freq(freq);
+
+  let amp = map(mouseY, 0, height, 1, 0.01);
+  wave.amp(amp);
+
+  ellipse(mouseX, mouseY, freq/10, amp * 100);
+
 }
